ede5cf7037a30ea2c68f43a70a3e02d7ab51ef10
Banja David Lotigo <innocdavid@gmail.com>
2023-07-29 23:25:15 +0300

    Update order model

--- store/models.py
+++ store/models.py
@@ -17,12 +17,12 @@ class Customer(models.Model):
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
-    PAYMENT_STATUS_COMPLETED = 'C'
+    PAYMENT_STATUS_COMPLETE = 'C'
     PAYMENT_STATUS_FAILED = 'F'
     PAYMENT_STATUS_CHOICES = [
-      ('P', 'Pending'), 
-      ('C', 'Complete'),
-      ('F', 'Failed')
+      (PAYMENT_STATUS_PENDING, 'Pending'), 
+      (PAYMENT_STATUS_COMPLETE, 'Complete'),
+      (PAYMENT_STATUS_FAILED, 'Failed')
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(

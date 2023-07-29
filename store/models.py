29d8b056e4171c7c289ca797975da0ffb722b2cf
Banja David Lotigo <innocdavid@gmail.com>
2023-07-29 23:06:29 +0300

    Update product model

--- store/models.py
+++ store/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
-    last_updated = models.DurationField(auto_now=True)
+    last_updated = models.DateTimeField(auto_now=True)
 
 class Customer(models.Model):
     first_name = models.CharField(max_length=255)

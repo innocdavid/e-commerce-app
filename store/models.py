b82190a4fc541837da5a49256514ca13ef2de749
Banja David Lotigo <innocdavid@gmail.com>
2023-07-29 23:02:47 +0300

    Feature: Create a customer model

--- store/models.py
+++ store/models.py
@@ -7,3 +7,10 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_updated = models.DurationField(auto_now=True)
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_day = models.DateField(null=True)

93a212baef0a8694a2330ac9c18ba736d364164f
Banja David Lotigo <innocdavid@gmail.com>
2023-07-31 22:05:54 +0300

    Feature: Create a cart item model class

--- store/models.py
+++ store/models.py
@@ -55,3 +55,8 @@ class Address(models.Model):
 
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()

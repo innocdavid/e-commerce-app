a1c4e55be71b90ea3902914884929d0252ad3bcc
Banja David Lotigo <innocdavid@gmail.com>
2023-07-31 22:03:04 +0300

    Feature: Create a cart model class

--- store/models.py
+++ store/models.py
@@ -52,3 +52,6 @@ class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
